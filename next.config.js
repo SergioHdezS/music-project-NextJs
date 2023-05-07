45d90d030cc4dd2a5139759ad2a8822bd9e4c3f1
SergioHdezS <sergiohdez.dev@gmail.com>
2023-05-06 20:33:06 -0400

    cambios para deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  // esta será la ruta con la que se reescribirán los enlaces 
+  basePath: '/music-project-spotify-clone',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
