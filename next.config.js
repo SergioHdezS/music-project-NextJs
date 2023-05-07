6cfe1c9d12db19ff89751da37a5f09e227769439
SergioHdezS <sergiohdez.dev@gmail.com>
2023-05-06 20:42:19 -0400

    remuevo export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   // esta será la ruta con la que se reescribirán los enlaces 
   basePath: '/music-project-spotify-clone',
   trailingSlash: true,
